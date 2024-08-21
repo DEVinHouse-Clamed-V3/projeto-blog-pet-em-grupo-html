c3ae8ad2b239bbfd1c2eed87f52b9e102e22b28b
Fabcuri <fabcuri70@gmail.com>
2024-08-21 20:50:05 -0300

    Criando pagina js dinamica e seu css

---
+++ scripts/homePosts.js
@@ -0,0 +1,38 @@
+function carregarDados() {
+    const postsNaMemoria = JSON.parse(localStorage.getItem("posts")) /*peguei a string do localstorage e transformei em array e guardei na variavel*/
+    const lista = document.getElementById("lista-posts")
+    
+    postsNaMemoria.forEach((post) => {
+        console.log(post.id)
+        const div = document.createElement("div")
+        div.classList.add("item-post")/*geração da div e atribuição da classe*/
+
+        const img = document.createElement("img")
+        img.setAttribute("width", "150px")
+        img.setAttribute("src", post.foto)
+
+        div.append(img)
+
+        const h3 = document.createElement("h3")
+        h3.innerText = post.tipo
+        div.append(h3)
+
+        const h2 = document.createElement("h2")
+        h2.innerText = post.descricao
+        div.append(h2)
+
+        const h5 = document.createElement("h5")
+        h5.innerText = post.dataCriacao
+        div.append(h5)
+
+        lista.append(div)
+
+    });
+
+
+
+
+}
+
+
+document.addEventListener("DOMContentLoaded", carregarDados) /*dom funciona quando a pagina for renderizada, dispara a função*/
